49e2fc7fc8cc0965b17b8606b9bc45095bc8c1d0
Vedant K <82938580+dopevog@users.noreply.github.com>
2021-05-22 21:56:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 setup(
     name='stox',
     packages=['stox'],
-    version='1',
+    version='1.00',
     description='Stock Price Prediction And Analysis',
     long_description=long_description,
     long_description_content_type="text/markdown",
